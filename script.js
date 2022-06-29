d06681b26d4131b44a398ab0ebc5dd0ec9db856e
RengarLOL <joshuataldrich@gmail.com>
2022-06-29 19:33:39 -0400

    added uppercase and lowercase letters to function

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
-// Assignment code here
-//create constant variables
-//create other variables
-//prompt length of password
-//prompt upper case letters
-//prompt lower case letters
+//set variables
+function generatePassword() {
+
+
+let characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}|:"<>?~,./;';
+let passwordLength = 8;
+let passwordValue = '';
+
+
+
 //prompt numbers 
 //prompt special characters
 //if not selected anything run again
-//generate password 
+//generate password
+
+//prompt length of password
+numberofCharacters = prompt("How many characters would you like in your password? Choose a number between 8-128.");
+if (numberofCharacters < 8 || numberofCharacters > 128) {
+  return "Please choose a number between 8-128.";
+}
+else if (isNaN(numberofCharacters)) {
+  numberofCharacters = prompt("Please pick a number between 8 and 128.");
+}
+else {
+  alert("You've chosen a password of" + numberofCharacters + " characters long.");
+}
+
+//prompt upper case letters
+letterUppercase = confirm("Do you want uppercase characters?");
+if (letterUppercase) {
+  var chooseUppercase = alert("Your password will include uppercase characters");
+}
+else {
+  alert("Your password will not include uppercase characters.");
+}
+
+
+//prompt lower case letters
+letterLowercase = confirm("Do you want lowercase characters?");
+if (letterLowercase) {
+  var chooseLowercase = alert("Your password will include lowercase characters.");
+}
+else {
+  alert("Your password will not include lowercase characters.");
+}
+
+
+
+
+
+
+
+
+
+
+//Create password
+const writePassword = () => {
+  passwordValue = '';
+
+  for (let i = 0; i < passwordLength; i++) {
+    let number = Math.floor(Math.random() * characters.length);
+    passwordValue += characters.substring(number, number + 1);
+  }
+
+  password.value = passwordValue;
+}
+
+
+//Events
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -25,21 +84,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function generatePassword() {
-
-}
-
-let characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+{}|:"<>?~';
-let passwordLength = 8;
-let passwordValue = '';
-
-
-//Create password
-const createPassword = () => {
-  passwordValue = '';
-
-  for (let i = 0; )
-}
-
-
